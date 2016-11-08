4feda2eb9f580f538dd8473afd332b24c74f98f6
Ann Lee <aplee29@users.noreply.github.com>
2016-11-08 17:49:34 -0500

    Done.

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log(this === window);
+// returns true
+
+function checkThis(){
+  console.log(this);
+}
+checkThis();
+// outputs window object
+
+function checkThis(){
+  'use strict';
+  console.log(this);
+}
+checkThis();
+// outputs "undefined"
+
+function Chair(style, color) {
+  console.log(this);
+  this.style = style;
+  this.color = color;
+}
+var sofa = new Chair("sofa", "green");
+
+var couch = {
+  color: 'green',
+  f: function() {
+    return this;
+  }
+};
+ 
+console.log(couch.f());
+// outputs object
